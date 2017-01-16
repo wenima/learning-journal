e6aa007e8e36d7de812c0d12db62c83a1b34c49e
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-16 08:24:51 -0800

    added twitter button and ajax base code

--- lj/models/mymodel.py
+++ lj/models/mymodel.py
@@ -16,5 +16,14 @@ class Post(Base):
     body = Column(Unicode)
     creation_date = Column(Date)
 
+    def to_json(self):
+        """Returns a Json type object."""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "creation_date": self.creation_date.strftime('%b %d, %Y'),
+        }
+
 
 Index('index', Post.title, unique=True, mysql_length=255)
